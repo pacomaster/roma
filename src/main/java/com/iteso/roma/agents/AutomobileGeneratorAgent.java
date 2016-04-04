3e5eaccdb7da01dd31affa80b99cb0056444e8bb
pacomaster <fco_amezcua@hotmail.com>
2016-04-04 09:31:54 -0500

    Senoidal simulation

--- src/main/java/com/iteso/roma/agents/AutomobileGeneratorAgent.java
+++ src/main/java/com/iteso/roma/agents/AutomobileGeneratorAgent.java
@@ -7,6 +7,8 @@ import jade.core.behaviours.TickerBehaviour;
 import jade.wrapper.AgentController;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
+import trasmapi.sumo.SumoCom;
+import trasmapi.sumo.SumoVehicle;
 
 /**
  * Class to generate automobile agents for simulation
@@ -26,6 +28,11 @@ public class AutomobileGeneratorAgent extends Agent{
 	private int val = 0;
 	private int inc = 1;
 	private int automobileCounter=1;
+	private ContainerController mainContainer;
+	
+	public AutomobileGeneratorAgent(ContainerController mainContainer) {
+		this.mainContainer = mainContainer;
+	}
 	
 	// ARGS
 	// (automobileGeneratorId, streetId,  timeToStart)
@@ -39,55 +46,6 @@ public class AutomobileGeneratorAgent extends Agent{
 		}
 		
 
-		// Behavior to wait for the generation of automobiles to start and then generate them
-		addBehaviour(new TickerBehaviour(this, TimeManager.getSeconds(1)) {
-			protected void onTick() {
-				if(timeToStart >=0){
-					if(timeToStart == 0){
-						
-						addBehaviour(new TickerBehaviour(myAgent, TimeManager.getSeconds(64)){
-							protected void onTick() {
-								val += inc;
-								if(val > 32){
-									inc = -1;
-									val -= 2;
-								}
-								if(val == 0){
-									inc = 1;
-									val += 2;
-								}
-								
-								int automobilesToCreate = val * 1;
-								//System.out.println("NUM: " + automobilesToCreate);
-								
-								for(int i = 0; i < automobilesToCreate; i++){
-									
-									int id = (ID * automobileGeneratorId) + automobileCounter;
-									Object[] objs = new Object[4];
-									objs[0] = id;
-									objs[1] = startStreetId;
-									objs[2] = endStreetId;
-									
-									ContainerController cc = getContainerController();
-									AgentController ac;
-									try {
-										ac = cc.createNewAgent("AutAge" + id, "com.iteso.roma.agents.AutomobileAgent", objs);
-										ac.start();
-									} catch (StaleProxyException e) {
-										e.printStackTrace();
-									}
-									
-									//System.out.println("Create AutAge" + id);
-									automobileCounter++;
-								}
-							}
-						});
-						timeToStart--;
-					}else{
-						timeToStart--;
-					}
-				}
-			}
-		});	
+		
 	}
 }
--- src/main/java/com/iteso/roma/agents/RomaManagerAgent.java
+++ src/main/java/com/iteso/roma/agents/RomaManagerAgent.java
@@ -3,17 +3,24 @@ package com.iteso.roma.agents;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.iteso.roma.utils.TimeManager;
+
 import jade.core.Agent;
 import jade.core.behaviours.TickerBehaviour;
 import jade.wrapper.ContainerController;
 import jade.wrapper.StaleProxyException;
 import trasmapi.sumo.SumoCom;
+import trasmapi.sumo.SumoVehicle;
 
 public class RomaManagerAgent extends Agent{
 	
 	private ContainerController mainContainer;
 	private int romaManagerId;
 	private Map<String, VehicleAgent> vehicleAgents =  new HashMap<String, VehicleAgent>(); 
+	
+	private int val = 0;
+	private int inc = 1;
+	private int automobileCounter=1;
 	
 	public RomaManagerAgent(int romaManagerId, ContainerController mainContainer) {
 		this.romaManagerId = romaManagerId;
@@ -26,22 +33,46 @@ public class RomaManagerAgent extends Agent{
 	
 	protected void setup() {
 		
-		addBehaviour(new TickerBehaviour(this, 1000) {
-			protected void onTick() {
-				for(String str : SumoCom.getAllVehiclesIds()){
-					if(vehicleAgents.get(str) == null){
-						VehicleAgent vehicle = new VehicleAgent(str);
-						try {
-							mainContainer.acceptNewAgent(str, vehicle).start();
-						} catch (StaleProxyException e) {
-							e.printStackTrace();
-						}
-						vehicleAgents.put(str, vehicle);
-					}
+		// Behavior to wait for the generation of automobiles to start and then generate them
+		addBehaviour(new TickerBehaviour(this, TimeManager.getSeconds(64)) {
+			protected void onTick() {								
+
+				val += inc;
+				if(val > 32){
+					inc = -1;
+					val -= 2;
 				}
-				System.out.println("Vehicles created so far: " + vehicleAgents.size());
+				if(val == 0){
+					inc = 1;
+					val += 2;
+				}
+				
+				int automobilesToCreate = val * 1;
+				//System.out.println("NUM: " + automobilesToCreate);
+				
+				for(int i = 0; i < automobilesToCreate; i++){
+					
+					int id = automobileCounter;
+					byte b = 0;
+					
+					SumoVehicle sumoVehicle = new SumoVehicle(id, "CarA", "route01", SumoCom.getCurrentSimStep(), 0, b);
+					SumoCom.addVehicleToSimulation(sumoVehicle);
+					
+					SumoCom.
+					
+					String str = "veh" + id;											
+					VehicleAgent vehicleAgent = new VehicleAgent(str);
+					try {
+						mainContainer.acceptNewAgent(str, vehicleAgent).start();
+					} catch (StaleProxyException e) {
+						e.printStackTrace();
+					}
+					
+					//System.out.println("Create AutAge" + id);
+					automobileCounter++;
+				}
 			}
-		});
+		});	
 		
 	}
 }
