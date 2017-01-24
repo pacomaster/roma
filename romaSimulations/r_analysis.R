66fb49edbe344a61e52c853b92d8bdad2be27b21
Francisco <fco_amezcua@hotmail.com>
2017-01-23 23:46:06 -0600

    Upload R analysis file with static and dynamic trips

--- romaSimulations/r_analysis.R
+++ romaSimulations/r_analysis.R
@@ -4,6 +4,7 @@ dynamicData <- read.csv("C:\\Users\\Francisco\\git\\roma\\romaSimulations\\data\
 staticData <- read.csv("C:\\Users\\famezcua\\PROJECTS\\Master ITESO\\sumo-0.25.0\\bin\\data\\trip.csv", stringsAsFactors=F)
 
 head(staticData)
+head(dynamicData)
 
 summary(staticData)
 summary(dynamicData)
@@ -14,6 +15,8 @@ dfStatic$timeLoss <- as.numeric(dfStatic$timeLoss)
 hlStatic <- dfStatic[dfStatic$departLane=="E5_0", ]
 
 dfDynamic <- data.frame(dynamicData)
+dfDynamic$duration <- as.numeric(dfDynamic$duration)
+dfDynamic$timeLoss <- as.numeric(dfDynamic$timeLoss)
 hlDynamic <- dfDynamic[dfDynamic$departLane=="E5_0", ]
 
 summary(hlStatic)
